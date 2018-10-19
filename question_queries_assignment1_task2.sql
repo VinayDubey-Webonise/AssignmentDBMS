9bf4cf7a7129032294a254c32f0c281e5abe3f58
VinayDubeyV-Webonise <43874875+VinayDubeyV-Webonise@users.noreply.github.com>
2018-10-19 15:26:28 +0530

    Update question_queries_assignment1_task2.sql

--- question_queries_assignment1_task2.sql
+++ question_queries_assignment1_task2.sql
@@ -1,8 +1,8 @@
 SELECT emp.name as "Employee Name",emp.age as "Age" from emp,dept,works where emp.id=works.emp_id and works.dept_id=dept.id and dept.name='hardware' or dept.name='software';
 
-
 select dept.manager_id as "Manager", GROUP_CONCAT(emp.name) as "Employee" from emp,works,dept WHERE emp.id=works.emp_id and works.dept_id= dept.id GROUP BY dept.manager_id;
 
+SELECT COUNT(emp.id) as "No. of employee", SUM(works.pct_time) as "Total work done" from dept,emp,works WHERE emp.id=works.emp_id and dept.id=works.dept_id GROUP BY works.dept_id HAVING  (SUM(works.pct_time)/100)>=10;
 
 select emp.name 
 from emp,dept,works 
