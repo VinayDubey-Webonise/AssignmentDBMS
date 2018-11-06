0d8c5fcd5476f544ab03edee5e4e2488eb6816b2
webonise <vinay.dubey@weboniselab.com>
2018-11-06 14:40:26 +0530

    Joins added in queries

--- question_queries_assignment1_task2.sql
+++ question_queries_assignment1_task2.sql
@@ -1,17 +1,52 @@
-SELECT emp.name as "Employee Name",emp.age as "Age" from emp,dept,works where emp.id=works.emp_id and works.dept_id=dept.id and dept.name='hardware' and dept.name='software';
+/* Question 1 */
 
-SELECT dept.manager_id as "Manager", GROUP_CONCAT(emp.name) as "Employee" from emp,works,dept WHERE emp.id=works.emp_id and works.dept_id= dept.id GROUP BY dept.manager_id;
+SELECT emp.name AS "Employee Name", emp.age AS "Age"
+FROM works 
+INNER JOIN emp ON emp.id=works.emp_id 
+INNER JOIN dept ON dept.id=works.dept_id 
+WHERE dept.name IN ("hardware","software") 
+GROUP BY works.emp_id 
+HAVING COUNT(*) = 2;
 
-SELECT COUNT(emp.id) as "No. of employee", SUM(works.pct_time) as "Total work done" from dept,emp,works WHERE emp.id=works.emp_id and dept.id=works.dept_id GROUP BY works.dept_id HAVING  (SUM(works.pct_time)/100)>=10;
+/* Question 2 */
 
-SELECT emp.name 
-from emp,dept,works 
-where emp.id=works.emp_id and works.dept_id=dept.id and 
-emp.salary> all (select max(dept.budget) from dept where dept.id=dept.id);
+SELECT dept.manager_id AS "Manager", GROUP_CONCAT(emp.name) AS "Employee"
+FROM works 
+INNER JOIN emp ON emp.id=works.emp_id 
+INNER JOIN dept ON dept.id=works.dept_id
+GROUP BY dept.manager_id;
+
+/* Question 3 */
+
+SELECT COUNT(emp.id) AS "No. of employee", SUM(works.pct_time) AS "Total work done"
+FROM works 
+INNER JOIN emp ON emp.id=works.emp_id 
+INNER JOIN dept ON dept.id=works.dept_id
+GROUP BY works.dept_id 
+HAVING (SUM(works.pct_time)/100)>=10;
+
+/* Question 4 */
+
+SELECT emp.name AS "Employee Name",emp.salary AS Salary,SUM(dept.budget) AS Budget
+FROM works 
+INNER JOIN emp ON emp.id=works.emp_id 
+INNER JOIN dept ON dept.id=works.dept_id 
+GROUP BY works.dept_id
+HAVING Salary>Budget;
+
+/* Question 5 */
+
+SELECT DISTINCT manager_id as "Manager Id"
+FROM dept 
+WHERE budget>1000000;
+
+/* Question 6 */
+
+SELECT emp.name AS "Manager Name", dept.budget AS Budget
+FROM works
+INNER JOIN emp ON emp.id=works.emp_id
+INNER JOIN dept ON dept.id=works.dept_id
+GROUP BY works.dept_id
+HAVING Budget=MAX(dept.budget);
 
 
-SELECT manager_id from dept 
-where budget >1000000;
-
-
-SELECT emp.name,manager_id,budget from dept,emp,works where emp.id = works.emp_id and dept.id = works.dept_id order by budget desc limit 2;
